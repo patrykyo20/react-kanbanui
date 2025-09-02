73650073869a7b1279e9e34f3ce0f6b3655275a8
Patryk <wierzbowskipatryk5@gmail.com>
2025-09-02 19:16:26 +0200

    Configure rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,46 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import dts from 'rollup-plugin-dts';
-import packageJson from './package.json';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+import { readFileSync } from "fs";
+
+const packageJson = JSON.parse(readFileSync("./package.json", "utf8"));
 
 export default [
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: [
       {
         file: packageJson.main,
-        format: 'cjs',
+        format: "cjs",
         sourcemap: true,
+        exports: "named",
       },
       {
         file: packageJson.module,
-        format: 'esm',
+        format: "esm",
         sourcemap: true,
+        exports: "named",
       },
     ],
     plugins: [
       peerDepsExternal(),
       resolve({
         browser: true,
+        preferBuiltins: false,
       }),
       commonjs(),
       typescript({
-        tsconfig: './tsconfig.json',
-        exclude: ['**/*.test.tsx', '**/*.test.ts', '**/*.stories.ts'],
+        tsconfig: "./tsconfig.app.json",
+        exclude: [
+          "**/*.test.tsx",
+          "**/*.test.ts",
+          "**/*.stories.ts",
+          "**/*.stories.tsx",
+          "node_modules/**",
+        ],
       }),
       postcss({
         modules: true,
@@ -37,11 +48,6 @@ export default [
         minimize: true,
       }),
     ],
-  },
-  {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
-    external: [/\.css$/],
+    external: ["react", "react-dom"],
   },
 ];
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export const hello = () => {
+  return "hello";
+};
+
+export const goodbye = () => {
+  return "goodbye";
+};
+
+export const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+export const subtract = (a: number, b: number) => {
+  return a - b;
+};
